b30814f5a315389492375f85db7710f203ae272a
Niko Bec <nikobec96@gmail.com>
2020-03-23 09:49:26 +0100

    added address search on gmaps

--- maplt.py
+++ maplt.py
@@ -1,3 +1,15 @@
-import webbrowser as wb
+#! python3
 
-wb.open("https://inventwithpython.com/")
+import webbrowser as wb
+import sys
+import pyperclip
+
+if len(sys.argv) > 1:
+    #sys.argv works as a list to store command line inputs
+    #get address from command line
+    address = ' '.join(sys.argv[1:])
+else:
+    #get address from clipboard
+    address = pyperclip.paste()
+
+wb.open("https://www.google.com/maps/search/" + address)
