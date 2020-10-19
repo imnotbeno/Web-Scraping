e7251f83d5d8c02c9beb395a135b5bc25471401a
Niko Bec <nikobec96@gmail.com>
2020-10-19 17:51:50 +0200

    Scraped the title and price

--- books_scraping.py
+++ books_scraping.py
@@ -5,6 +5,10 @@ from bs4 import BeautifulSoup
 # Request URL
 response = requests.get("http://books.toscrape.com/catalogue/category/books/science_22/index.html")
 # Init BS
-BeautifulSoup(response.text, "html.parser")
-
+soup = BeautifulSoup(response.text, "html.parser")
+# Book titles
+books = soup.find_all("article")
+for book in books: 
+    title = book.find("h3").find("a")["title"]
+    price = float(price.replace("£","").replace("Â",""))
 
