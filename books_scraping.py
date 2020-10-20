09d818e2bf00da54dd4afff3f51b2d7a853209bb
Niko Bec <nikobec96@gmail.com>
2020-10-20 08:30:49 +0200

    Divided the code into seperate methods

--- books_scraping.py
+++ books_scraping.py
@@ -6,15 +6,34 @@ from bs4 import BeautifulSoup
 response = requests.get("http://books.toscrape.com/catalogue/category/books/science_22/index.html")
 # Init BS
 soup = BeautifulSoup(response.text, "html.parser")
-# Book titles
+# Scrape book info
 books = soup.find_all("article")
-for book in books: 
-    #title = book.find("h3").find("a")["title"]
-    #price = book.select(".price_color")[0].get_text()
-    #price = float(price.replace("£","").replace("Â",""))
-    ratings = {"Zero":0, "One":1, "Two":2, "Three":3, "Four":4, "Five":5}
-    paragraph = book.select(".star-rating")[0]
-    rating = paragraph.get_attribute_list("class") [-1] #we want the last item
-    int_rating = ratings[rating]
-    print(int_rating)
 
+# Method to extract the book title
+def get_title():
+    for book in books:
+        title = book.find("h3").find("a")["title"]
+        return title
+
+# Method to extract book price
+def get_price():
+    for book in books:
+        price_text = book.select(".price_color")[0].text
+        price = float(price_text.replace("Â","").replace("£",""))
+        return price
+
+# Method to extract book rating
+def get_rating():
+    ratings = {"Zero":0, "One":1, "Two":2, "Three":3, "Four":4, "Five":5}
+    for book in books:
+        p = book.select(".star-rating")[0] 
+        rating = p.get_attribute_list("class") [-1]
+        int_rating = ratings[rating]
+        return int_rating
+
+
+# Driver code
+if __name__ == "__main__":
+    get_title()
+    get_price()
+    get_rating()
