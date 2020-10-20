6d9527e9dac043e134ff6faab4d509307e7f65d6
Niko Bec <nikobec96@gmail.com>
2020-10-20 07:55:25 +0200

    Scraping the rating and converting into integers

--- books_scraping.py
+++ books_scraping.py
@@ -9,6 +9,12 @@ soup = BeautifulSoup(response.text, "html.parser")
 # Book titles
 books = soup.find_all("article")
 for book in books: 
-    title = book.find("h3").find("a")["title"]
-    price = float(price.replace("£","").replace("Â",""))
+    #title = book.find("h3").find("a")["title"]
+    #price = book.select(".price_color")[0].get_text()
+    #price = float(price.replace("£","").replace("Â",""))
+    ratings = {"Zero":0, "One":1, "Two":2, "Three":3, "Four":4, "Five":5}
+    paragraph = book.select(".star-rating")[0]
+    rating = paragraph.get_attribute_list("class") [-1] #we want the last item
+    int_rating = ratings[rating]
+    print(int_rating)
 
