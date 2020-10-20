3c8253223e3def93b06bf2c27f4f13ba77113434
Niko Bec <nikobec96@gmail.com>
2020-10-20 08:48:07 +0200

    Divided into more methods and cleaned up the code

--- books_scraping.py
+++ books_scraping.py
@@ -2,38 +2,42 @@ import requests
 import sqlite3
 from bs4 import BeautifulSoup
 
-# Request URL
-response = requests.get("http://books.toscrape.com/catalogue/category/books/science_22/index.html")
-# Init BS
-soup = BeautifulSoup(response.text, "html.parser")
-# Scrape book info
-books = soup.find_all("article")
+def scrape_books(url):
+
+    # Request URL
+    response = requests.get(url)
+
+    #Init BeautifulSoup
+    soup = BeautifulSoup(response.text, "html.parser")
+
+    # Scrape book info
+    books = soup.find_all("article")
+    
+    # Iterate through all books and append all book data to a list
+    all_books = []
+    for book in books:
+        book_data = (get_title(book), get_price(book), get_rating(book))
+        all_books.append(book_data)
+        print(all_books)
 
 # Method to extract the book title
-def get_title():
-    for book in books:
-        title = book.find("h3").find("a")["title"]
-        return title
+def get_title(book):
+    title = book.find("h3").find("a")["title"]
+    return title
 
 # Method to extract book price
-def get_price():
-    for book in books:
-        price_text = book.select(".price_color")[0].text
-        price = float(price_text.replace("Â","").replace("£",""))
-        return price
+def get_price(book):
+    price_text = book.select(".price_color")[0].text
+    price = float(price_text.replace("Â","").replace("£",""))
+    return price
 
 # Method to extract book rating
-def get_rating():
+def get_rating(book):
     ratings = {"Zero":0, "One":1, "Two":2, "Three":3, "Four":4, "Five":5}
-    for book in books:
-        p = book.select(".star-rating")[0] 
-        rating = p.get_attribute_list("class") [-1]
-        int_rating = ratings[rating]
-        return int_rating
-
+    p = book.select(".star-rating")[0] 
+    word = p.get_attribute_list("class") [-1]
+    return ratings[word]
 
 # Driver code
 if __name__ == "__main__":
-    get_title()
-    get_price()
-    get_rating()
+    scrape_books("http://books.toscrape.com/catalogue/category/books/science_22/index.html")
